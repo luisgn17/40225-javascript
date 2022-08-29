c6378292cf1efa80fb3daadc42802a74eacbd574
luisgn17 <luisnava141@gmail.com>
2022-08-29 10:28:45 -0300

    interaccion usuario

--- js/index.js
+++ js/index.js
@@ -1,53 +1,62 @@
-let  n1, n2, r;
-        n1 = parseInt(prompt ("Ingrese primer Numero"));
-        n2 = parseInt(prompt ("Ingrese segundo Numero"));
-        r = n1 + n2;
-alert ("el resultado de la suma es :"+r);
-let n3;
-n3 = parseInt(prompt ("Ingrese tercer Numero"));
-r=n3*r;
-alert ("el resultado la multiplicacion es :"+r);
-alert ("Bienvenido Ahora Ingresa tus datos");
-let nom, ape, ed, re;
-do{
-    nom = (prompt ("Ingrese su Nombre"));
-}while(nom !=='luis')
-alert ("Bienvenido :"+nom);
-ape = (prompt ("ingrese su Apellido "));
-ed = (prompt ("Ingrese su edad"));
-if (ed < 18){
-    alert ("la edad no coincide con la base de datos");
-    }else if(ed > 18){
-    alert ("la edad coincide");
-}
-alert ("continua :"+nom);
-let  n4, n5, res;
-        n4 = parseInt(prompt ("Ingrese primer Numero"));
-        n5 = parseInt(prompt ("Ingrese segundo Numero"));
-        res = n4 + n5;
-console.log ("el resultado de la suma es :"+res);
-let n6;
-n6 = parseInt(prompt ("Ingrese tercer Numero"));
-res=n6*res;
-console.log ("el resultado la multiplicacion es :"+res);
-let n7, n9;
-n7 = prompt("Dime un numero");
-n9 = prompt("Dime otro numero");
-document.write("La suma de "+ n7 + " y "+ n9 + " es: ");
-document.write(parseInt(n7)+parseInt(n9));
-let x = 0;
-let z = 0;
-labelCancelLoops: while (true) {
-console.log('Bucles externos: ' + x);
-x += 1;
-z = 1;
-while (true) {
-    document.write('bucles repetido: ' + z);
-    z += 1;
-    if (z === 10 && x === 10) {
-    break labelCancelLoops;
-    } else if (z === 10) {
-    break;
-    }
-}
-}
+
+// function sumar(){
+//     let numero1 = parseInt(prompt('ingresa un numero'))
+//     let numero2 = parseInt(prompt('ingresa otro numero'))
+    
+//     const resultado = numero1 + numero2
+//     alert('el resultado es'+resultado)
+//     return
+// }
+// let  n1, n2, r;
+//         n1 = parseInt(prompt ("Ingrese primer Numero"));
+//         n2 = parseInt(prompt ("Ingrese segundo Numero"));
+//         r = n1 + n2;
+// alert ("el resultado de la suma es :"+r);
+// let n3;
+// n3 = parseInt(prompt ("Ingrese tercer Numero"));
+// r=n3*r;
+// alert ("el resultado la multiplicacion es :"+r);
+// alert ("Bienvenido Ahora Ingresa tus datos");
+// let nom, ape, ed, re;
+// do{
+//     nom = (prompt ("Ingrese su Nombre"));
+// }while(nom !=='luis')
+// alert ("Bienvenido :"+nom);
+// ape = (prompt ("ingrese su Apellido "));
+// ed = (prompt ("Ingrese su edad"));
+// if (ed < 18){
+//     alert ("la edad no coincide con la base de datos");
+//     }else if(ed > 18){
+//     alert ("la edad coincide");
+// }
+// alert ("continua :"+nom);
+// let  n4, n5, res;
+//         n4 = parseInt(prompt ("Ingrese primer Numero"));
+//         n5 = parseInt(prompt ("Ingrese segundo Numero"));
+//         res = n4 + n5;
+// console.log ("el resultado de la suma es :"+res);
+// let n6;
+// n6 = parseInt(prompt ("Ingrese tercer Numero"));
+// res=n6*res;
+// console.log ("el resultado la multiplicacion es :"+res);
+// let n7, n9;
+// n7 = prompt("Dime un numero");
+// n9 = prompt("Dime otro numero");
+// document.write("La suma de "+ n7 + " y "+ n9 + " es: ");
+// document.write(parseInt(n7)+parseInt(n9));
+// let x = 0;
+// let z = 0;
+// labelCancelLoops: while (true) {
+// console.log('Bucles externos: ' + x);
+// x += 1;
+// z = 1;
+// while (true) {
+//     document.write('bucles repetido: ' + z);
+//     z += 1;
+//     if (z === 10 && x === 10) {
+//     break labelCancelLoops;
+//     } else if (z === 10) {
+//     break;
+//     }
+// }
+// }
