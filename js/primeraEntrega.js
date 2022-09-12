f012e0af305c88702704b0a18a170c8382e0495f
luisgn17 <luisnava141@gmail.com>
2022-09-12 19:21:33 -0300

    ecomerce

--- js/primeraEntrega.js
+++ js/primeraEntrega.js
@@ -1,124 +1,95 @@
-class menu {
-  constructor(nombre, id) {
-    this.nombre = nombre
-    this.id = id
-    this.comida = [
-      {
-        nombre: 'Simple',
-        impuestos: [
-          {
-          precio: 850,
-          iva: this.generarIva(),
-          },
-          {
-          precio: 950,
-          iva: this.generarIva(),
-          },
-          {
-          precio: 1050,
-          iva: this.generarIva(),
-          },
-        ],
-      },
-      {
-        nombre: 'Doble',
-        impuestos: [
-          {
-              precio: 1150,
-              iva: this.generarIva(),
-          },
-          {
-              precio: 1250,
-              iva: this.generarIva(),
-          },
-          {
-              precio: 1350,
-              iva: this.generarIva(),
-          },
-        ],
-      },
-      {
-        nombre: 'Triple',
-        impuestos: [
-          {
-              precio: 1450,
-              iva: this.generarIva(),
-          },
-          {
-              precio: 1550,
-              iva: this.generarIva(),
-          },
-          {
-              precio: 1650,
-              iva: this.generarIva(),
-          },
-        ],
-      },
-    ]
-  }
-
-  generarIva() {
-    return Math.floor(Math.random() * 20 + 1)
-  }
-
-  anadirComida(nombre){
-    const hamburg = {nombre:nombre}
-    this.comida.push(hamburg)
-  }
-}
-const newMenu = []
-const nuevaOrden = new menu('Mesa', 1)
-nuevaOrden.anadirComida('Cuadruple')
-newMenu.push(nuevaOrden)
-const nuevaOrden2 = new menu('Quinta', 2)
-newMenu.push(nuevaOrden2)
-const libre = new menu('libre', 3)
-newMenu.push(libre)
-console.log(newMenu)
-
-const tipoMenuSeleccionado = parseInt(
-  prompt('Ingrese el tipo de menu: 1. Menu 1 - 2. Menu 2 - 3.libre')
-)
-const existeTipoDeMenu = newMenu.find(
-  (pres) => pres.id === tipoMenuSeleccionado
-)
-console.log(existeTipoDeMenu)
-
-const IvaSeleccion = parseInt(
-  prompt('A caul iva deseas pagar el Menu?: 6, 12 o 28')
-)
-const comida = existeTipoDeMenu.comida
-console.log(comida)
-
-
-const arrayIvaSegunSeleccionado = []
-comida.forEach((comida) => {
-  const ivaArray = hamburg.impuestos
-  console.log(ivaArray)
-  const ivaPorMenu = ivaArray.find(
-    (impuestos) => impuestos.precio === ivaMenu
-  )
-  console.log(ivaPorMenu)
-  arrayIvaSegunSeleccionado.push({
-    nombre: hamburg.nombre,
-    iva: ivaPorMenu.iva,
-  })
+const contenedorProductos = document.getElementById('contenedor-productos')
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+const botonVaciar = document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('Precio-Total')
+const cantidad = document.getElementById('cantidad')
+const cantidadTotal = document.getElementById('cantidadTotal')
+let carrito = []
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        // renovarCarrito()
+    }
 })
-console.log(ivaArray)
-console.log(arrayIvaSegunSeleccionado)
+let stockProductos = [
+    {id: 1, nombre: "cera depilatoria Rep", Tipo:"cera", cantidad: 1, desc: "Cera", precio: 300, tamaño:"Peq", img:'./Depimiel/1.jpg'},
+    {id: 2, nombre: "algabo 1", Tipo:"cera", cantidad: 1, desc: "alcohol en gel", precio: 300, tamaño:"l", img:'./Depimiel/2.png'},
+    {id: 3, nombre: "algabo 1", Tipo:"cera", cantidad: 1, desc: "alcohol en gel", precio: 300, tamaño:"l", img:'./Depimiel/3.png'},
+    {id: 4, nombre: "algabo 1", Tipo:"cera", cantidad: 1, desc: "alcohol en gel", precio: 300, tamaño:"l", img:'./Depimiel/4.png'},
+    {id: 5, nombre: "algabo 1", Tipo:"cera", cantidad: 1, desc: "alcohol en gel", precio: 300, tamaño:"l", img:'./Depimiel/5.png'},
+    {id: 6, nombre: "algabo 1", Tipo:"cera", cantidad: 1, desc: "alcohol en gel", precio: 300, tamaño:"l", img:'./Depimiel/6.png'},
+    {id: 7, nombre: "algabo 1", Tipo:"cera", cantidad: 1, desc: "alcohol en gel", precio: 300, tamaño:"l", img:'./Depimiel/7.png'},
+    {id: 8, nombre: "algabo 1", Tipo:"cera", cantidad: 1, desc: "alcohol en gel", precio: 300, tamaño:"l", img:'./Depimiel/8.png'},
+    {id: 9, nombre: "algabo 1", Tipo:"cera", cantidad: 1, desc: "alcohol en gel", precio: 300, tamaño:"l", img:'./Depimiel/9.png'},
+    {id: 10, nombre: "algabo 1", Tipo:"cera", cantidad: 1, desc: "alcohol en gel", precio: 300, tamaño:"l", img:'./Depimiel/10.png'}];
 
-arrayIvaSegunSeleccionado.sort((a, b) => a.iva - b.iva)
-console.log(arrayIvaSegunSeleccionado)
-const mejorOpcion = arrayIvaSegunSeleccionado[0]
-console.log(mejorOpcion)
 
-const CuantoPagaras = parseInt(prompt('Ingrese el monto que pagar: '))
+// botonVaciar.addEventListener('click', () => {
+//     carrito.length = 0
+//     renovarCarrito()
+// })
 
-const CuotaPaga = CuantoPagaras / IvaSeleccion
-const TotalPagar =
-CuotaPaga * (mejorOpcion.impuestos/ 100)
-const cuotaTotal = CuotaPaga + TotalPagar
+stockProductos.forEach((producto) => {
+  const div = document.createElement('div')
+  div.classList.add('producto')
+  div.innerHTML = `
+  <img src=${producto.img} alt="">
+  <h3>${producto.nombre}</h3>
+  <p>${producto.desc}</p>
+  <p>Tamaño: ${producto.tamaño}</p>
+  <p class="precioProducto">Precio:$ ${producto.precio}</p>
+  <button id="agregar${producto.id}" class=boton-agregar">Agregar</button>
+  `
+  contenedorProductos.appendChild(div)
 
-alert(
-  `El nejor precio que ofrece es ${mejorOpcion.nombre} con un valor de $${cuotaTotal} por un pago`
-)
+const boton = document.getElementById(`agregar${producto.id}`)
+
+boton.addEventListener('click', () => {
+    agregarAlCarrito(producto.id)
+})  
+})
+const agregarAlCarrito = (prodId) => {
+    const existe = carrito.some (prod => prod.id === prodId)
+    
+    if (existe){
+        const prod = carrito.map (prod => {
+            if (prod.id === prodId) {
+                prod.cantidad++
+            }
+        })
+    } else {
+    const item = stockProductos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    console.log(carrito)
+}
+renovarCarrito()
+}
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId) 
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+    renovarCarrito()
+    console.log(carrito)
+}
+
+
+
+const renovarCarrito = () => { 
+    contenedorCarrito.innerHTML = ""
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnElCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio: ${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick = "eliminarDelCarrito(${prod.id})" class="boton-eliminar"></button>
+        `
+        contenedorCarrito.appendChild(div)
+        localStorage.setItem('carrito',JSON.stringify(carrito))
+    })
+    contadorCarrito.innerText = carrito.length
+    console.log(carrito)
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+}
