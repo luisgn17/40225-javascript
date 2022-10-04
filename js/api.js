9fee79d6bcde84730b41410efb9fc7b75c1fc304
luisgn17 <luisnava141@gmail.com>
2022-10-04 10:44:32 -0300

    api datos covid

---
+++ js/api.js
@@ -0,0 +1,21 @@
+const url = 'https://api.covidtracking.com/v1/us/20200501.json'
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    let element = document.getElementById('elem')
+    element.innerHTML = `
+    Datos Covid-19 Estados Unidos
+    <p>Fecha: ${data.dateChecked}</p>
+    <p>Muertes: ${data.death}</p>
+    <p>Hospitalizados: ${data.hospitalized}</p>
+    <p>Casos Negativos: ${data.negative}</p>
+    <p>Falsos Positivos: ${data.pending}</p>
+    <p>Casos Positivos: ${data.positive}</p>
+    <p>Estados: ${data.states}</p>
+
+    <p>Total Resultado Test Covid 19: ${data.totalTestResults}</p>  
+    `
+    console.log(data)
+})
+.catch(err => console.log(err))
